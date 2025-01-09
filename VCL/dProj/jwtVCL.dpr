1e5f14a56b6b40b08b47183692e1f4769c978a14
Fábio Ghizoni <fabiohenriqueghizoni10@gmail.com>
2025-01-09 17:54:21 -0300

    Initial commit

---
+++ VCL/dProj/jwtVCL.dpr
@@ -0,0 +1,14 @@
+program jwtVCL;
+
+uses
+  Vcl.Forms,
+  uMain in '..\Units\uMain.pas' {frmMain};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TfrmMain, frmMain);
+  Application.Run;
+end.
